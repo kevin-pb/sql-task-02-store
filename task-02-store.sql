dac0351195568be6c19552072a95c1ac47668bab
USER <kevinpb.py@gmail.com>
2024-08-08 20:22:20 +0200

    code restructure

--- task-02-store.sql
+++ task-02-store.sql
@@ -1,82 +1,3 @@
-/*
-pre exercise
-*/
-use store ;
-
-create table employee (
-employee_id int primary key,
-last_name varchar(40),
-first_name varchar(20),
-birth_date date not null,
-hire_date date not null,
-address varchar(128) not null,
-city varchar(30) not null,
-counttry varchar(30) not null,
-reorts_to int not null
-);
-
-ALTER TABLE employee ADD CONSTRAINT FOREIGN KEY (`reorts_to`) REFERENCES employee (`employee_id`);
-
-create table purchase (
-purchase_id int primary key, 
-customer_id int, 
-employee_id int,
-total_price decimal(10.2),
-purchase_data timestamp not null,
-shipped_data timestamp not null,
-ship_addres varchar(60) not null,
-ship_city varchar(15) not null,
-ship_counttry varchar(15) not null
-);
-
-ALTER TABLE purchase ADD CONSTRAINT FOREIGN KEY (`employee_id`) REFERENCES employee (`employee_id`);
-
-create table customer (
-customer_id int primary key, 
-contact_name varchar(30), 
-company_name varchar(40) not null,
-contact_email varchar(128),
-address varchar(120) not null,
-city varchar(30) not null,
-country varchar(30) not null
-);
-
-ALTER TABLE purchase ADD CONSTRAINT FOREIGN KEY (`customer_id`) REFERENCES customer (`customer_id`);
-
-create table purchase_item (
-purchase_id int, 
-product_id int, 
-unit_price decimal(10.2) not null,
-quantity int,
-primary key (purchase_id, product_id)
-);
-
-
-ALTER TABLE purchase_item ADD CONSTRAINT FOREIGN KEY (`purchase_id`) REFERENCES purchase (`purchase_id`);
-
-create table product (
-product_id int primary key, 
-product_name varchar(45), 
-category_id int,
-quantity_per_unit int,
-unit_price decimal(10.2) not null,
-units_in_stock int not null,
-discontinued boolean not null
-);
-
-ALTER TABLE purchase_item ADD CONSTRAINT FOREIGN KEY (`product_id`) REFERENCES product (`product_id`);
-
-create table category (
-category_id  int primary key, 
-name varchar(60) , 
-description text not null,
-parent_category_id int not null
-);
-
-ALTER TABLE category ADD CONSTRAINT FOREIGN KEY (`parent_category_id`) REFERENCES category (`category_id`);
-
-ALTER TABLE product ADD CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES category (`category_id`);
-
 /**
  * Exercise #7
  */
@@ -85,19 +6,23 @@ select * from product
 /**
  * Exercise #7
  */
-SELECT product_name FROM product WHERE unit_price >= 3.5;
+SELECT product_name 
+FROM product 
+WHERE unit_price >= 3.5;
 
 /**
  * Exercise #7
  */
-SELECT * FROM store.product where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
+SELECT * 
+FROM store.product 
+where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
 
 /**
  * Exercise #7
  */
-select p.product_name, c.category_name from product as p
-inner join category as c
-on p.product_id =  c.category_id;
+select p.product_name, c.category_name 
+from product as p 
+inner join category as c on p.product_id =  c.category_id;
 
 /**
  * Exercise #7
@@ -116,29 +41,34 @@ inner join product as p on c.category_id = p.product_id
 /**
  * Exercise #7
  */
-select * from category 
+select * 
+from category 
 order by category_name asc;
 
 /**
  * Exercise #7
  */
-select first_name, last_name, birth_date from employee 
+select first_name, last_name, birth_date 
+from employee 
 order by birth_date desc;
 
 /**
  * Exercise #7
  */
-select * from product as p
+select * 
+from product as p
 order by quantity_per_unit desc, product_name asc;
 
 /**
  * Exercise #7
  */
-select product_name, avg(unit_price) FROM product
+select product_name, avg(unit_price) 
+FROM product
 group by product_name
 
 /**
  * Exercise #7
  */
-select city, count(customer_id) from customer
+select city, count(customer_id) 
+from customer
 group by city;

203963bf898bd56d2d31857cfa29e0d5ae8be02b
USER <kevinpb.py@gmail.com>
2024-08-08 19:46:03 +0200

    ad er model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-a
+
+## Entity-Relationship (ER) model 
+![model.er](./rsc/model.er.drawio.svg)
--- task-02-store.sql
+++ task-02-store.sql
@@ -86,13 +86,13 @@ select * from product
 /*
 exercise 2
 */
-SELECT product_name FROM product WHERE unit_price <= 3.5;
+SELECT product_name FROM product WHERE unit_price >= 3.5;
 
 /*
 exercise 3
 */
 
-SELECT * FROM store.product where product_id = 1 or product_id = 5;
+SELECT * FROM store.product where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
 
 /*
 exercise 4
@@ -103,22 +103,51 @@ inner join category as c
 on p.product_id =  c.category_id;
 
 /*
-exercise 7
+exercise 5
 */
 
-select * from category as e
+select pu.purchase_id, p.product_name, pu_i.unit_price, pu_i.quantity 
+from product as p
+inner join purchase_item as pu_i on p.product_id = pu_i.product_id
+inner join  purchase as pu on pu.purchase_id =  pu_i.purchase_id;
+/*
+exercise 6
+*/
+
+select p.category_id, c.category_name 
+from category as c
+inner join product as p on c.category_id = p.product_id
+
+/**
+ * Exercise #7
+ */
+select * from category 
 order by category_name asc;
 
 /*
 exercise 8
 */
 
-select * from employee as e
-order by birth_date asc;
+select first_name, last_name, birth_date from employee 
+order by birth_date desc;
 
 /*
-exercise 8
+exercise 9
 */
 
 select * from product as p
 order by quantity_per_unit desc, product_name asc;
+
+/*
+exercise 10
+*/
+
+select product_name, avg(unit_price) FROM product
+group by product_name
+
+/*
+exercise 11
+*/
+
+select city, count(customer_id) from customer
+group by city;

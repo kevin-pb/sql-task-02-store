25637600192c9f8585d319348ccd3de3b50a827c
USER <kevinpb.py@gmail.com>
2024-08-08 20:18:51 +0200

    restructure

--- task-02-store.sql
+++ task-02-store.sql
@@ -77,43 +77,38 @@ ALTER TABLE category ADD CONSTRAINT FOREIGN KEY (`parent_category_id`) REFERENCE
 
 ALTER TABLE product ADD CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES category (`category_id`);
 
-/*
-exercise 1
-*/
-
+/**
+ * Exercise #7
+ */
 select * from product
 
-/*
-exercise 2
-*/
+/**
+ * Exercise #7
+ */
 SELECT product_name FROM product WHERE unit_price >= 3.5;
 
-/*
-exercise 3
-*/
-
+/**
+ * Exercise #7
+ */
 SELECT * FROM store.product where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
 
-/*
-exercise 4
-*/
-
+/**
+ * Exercise #7
+ */
 select p.product_name, c.category_name from product as p
 inner join category as c
 on p.product_id =  c.category_id;
 
-/*
-exercise 5
-*/
-
+/**
+ * Exercise #7
+ */
 select pu.purchase_id, p.product_name, pu_i.unit_price, pu_i.quantity 
 from product as p
 inner join purchase_item as pu_i on p.product_id = pu_i.product_id
 inner join  purchase as pu on pu.purchase_id =  pu_i.purchase_id;
-/*
-exercise 6
-*/
-
+/**
+ * Exercise #7
+ */
 select p.category_id, c.category_name 
 from category as c
 inner join product as p on c.category_id = p.product_id
@@ -124,30 +119,26 @@ inner join product as p on c.category_id = p.product_id
 select * from category 
 order by category_name asc;
 
-/*
-exercise 8
-*/
-
+/**
+ * Exercise #7
+ */
 select first_name, last_name, birth_date from employee 
 order by birth_date desc;
 
-/*
-exercise 9
-*/
-
+/**
+ * Exercise #7
+ */
 select * from product as p
 order by quantity_per_unit desc, product_name asc;
 
-/*
-exercise 10
-*/
-
+/**
+ * Exercise #7
+ */
 select product_name, avg(unit_price) FROM product
 group by product_name
 
-/*
-exercise 11
-*/
-
+/**
+ * Exercise #7
+ */
 select city, count(customer_id) from customer
 group by city;

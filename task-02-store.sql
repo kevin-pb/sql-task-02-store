cef772313e1e695633b0bf5b8b3a136937dbb382
USER <kevinpb.py@gmail.com>
2024-08-08 20:26:55 +0200

    restructure the code

--- task-02-store.sql
+++ task-02-store.sql
@@ -1,38 +1,39 @@
 /**
- * Exercise #7
+ * Exercise #1
  */
-select * from product
+select * 
+from product
 
 /**
- * Exercise #7
+ * Exercise #2
  */
 SELECT product_name 
 FROM product 
 WHERE unit_price >= 3.5;
 
 /**
- * Exercise #7
+ * Exercise #3
  */
 SELECT * 
 FROM store.product 
 where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
 
 /**
- * Exercise #7
+ * Exercise #4
  */
 select p.product_name, c.category_name 
 from product as p 
 inner join category as c on p.product_id =  c.category_id;
 
 /**
- * Exercise #7
+ * Exercise #5
  */
 select pu.purchase_id, p.product_name, pu_i.unit_price, pu_i.quantity 
 from product as p
 inner join purchase_item as pu_i on p.product_id = pu_i.product_id
 inner join  purchase as pu on pu.purchase_id =  pu_i.purchase_id;
 /**
- * Exercise #7
+ * Exercise #6
  */
 select p.category_id, c.category_name 
 from category as c
@@ -46,29 +47,30 @@ from category
 order by category_name asc;
 
 /**
- * Exercise #7
+ * Exercise #8
  */
 select first_name, last_name, birth_date 
 from employee 
 order by birth_date desc;
 
 /**
- * Exercise #7
+ * Exercise #9
  */
 select * 
 from product as p
 order by quantity_per_unit desc, product_name asc;
 
 /**
- * Exercise #7
+ * Exercise #10
  */
 select product_name, avg(unit_price) 
 FROM product
 group by product_name
 
 /**
- * Exercise #7
+ * Exercise #11
  */
-select city, count(customer_id) 
-from customer
-group by city;
+select c.city, count(customer_id) as total
+from store.customer AS c
+where c.city != "Knoxville"
+group by c.city;

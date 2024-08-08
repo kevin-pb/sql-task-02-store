6a599df1b5d83b79c71bf6fe8ef43f4ab147cd13
USER <kevinpb.py@gmail.com>
2024-08-08 21:44:00 +0200

    update the code

---
+++ init.02.seeder.sql
@@ -0,0 +1,26 @@
+insert into employee 
+values
+(1, 'Perez Borges', 'Kevin','2010-06-14', '2020-06-17','El Marabu 29', 'Stockton', 'Germany',null),
+(2, 'Membrides Borges', 'Yanet','2011-06-14', '2021-06-17','El Clot 29', 'Stockton', 'Germany',null),
+(3, 'Perez Espinosa', 'Natalie','2012-06-14', '2022-06-17','El Poblenou 29', 'Knoxville ', 'Germany',null),
+(4, 'Lezcano Borges', 'Antonio','2013-06-14', '2023-06-17','Pueblo Grifo 29', 'Stockton', 'Germany',null),
+(5, 'Perez Marques', 'Nora','2014-06-14', '2024-06-17','Richard 29', 'Boston', 'EU',null),
+(6, 'Chico Borges', 'Noa','2010-07-14', '2020-06-17','El Marabu 45', 'Barcelona', 'España',null),
+(7, 'Perez Carrasana', 'Ainoa','2017-06-14', '2021-06-17','El Marabu 12', 'Madrid', 'España',null),
+(8, 'Rodriges Borges', 'Juan','2010-07-14', '2022-06-17','El Marabu 25', 'Miami', 'EU',1),
+(9, 'Perez Perez', 'Raul','2010-08-14', '2023-06-17','El Marabu 36', 'Oviedo', 'España',1),
+(10, 'Borges Borges', 'Gusbey','2010-06-16', '2024-06-17','El Marabu 29', 'Stockton', 'Germany',3);
+
+
+insert into employee 
+values
+(1, 'Food', 'High-quality food products, ranging from everyday essentials to gourmet items, catering to all your culinary tastes and needs.', null),
+(2, 'Apliances', 'Discover a wide range of innovative appliances that make your life easier, from large kitchen equipment to essential household gadgets.',null),
+(3, 'Furnitures', 'Discover a wide range of innovative appliances that make your life easier, from large kitchen equipment to essential household gadgets.',null),
+(4, 'Clothes', 'Fashionable clothing for every occasion, offering a selection that ranges from casual wear to formal attire, designed to express your personal style.',null),
+(5, 'Fruits and vegetables', 'Fresh fruits and vegetables, carefully grown and selected to provide you with the best nature has to offer each season.',1),
+(6, 'Shoes', 'High-quality footwear for all ages and styles, from comfortable everyday shoes to elegant options for special occasions.',null),
+(7, 'Jewelery', 'Exquisite jewelry that enhances your beauty and elegance, featuring unique designs and top-quality materials for every taste and occasion.',null),
+(8, 'Home', 'Everything you need to turn your house into a home, with a wide range of products that combine style and functionality for every corner.',null),
+(9, 'Clean', 'Effective and safe cleaning products to keep your home spotless and protected, designed to care for both your environment and your health.',8),
+(10, 'Skin care', 'Skincare products that nourish, protect, and enhance your skin, formulated with natural ingredients and advanced technology for all skin types.',null);

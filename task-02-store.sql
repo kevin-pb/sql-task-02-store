3ff3898cbc411c4cc0b4763d1cfd9eca9443e527
USER <kevinpb.py@gmail.com>
2024-08-08 23:00:54 +0200

    variated changes

--- task-02-store.sql
+++ task-02-store.sql
@@ -2,7 +2,7 @@
  * Exercise #1
  */
 select * 
-from product
+from product;
 
 /**
  * Exercise #2
@@ -21,9 +21,9 @@ where (category_id = 1 or category_id = 3) and unit_price >= 3.5;
 /**
  * Exercise #4
  */
-select p.product_name, c.category_name 
+select p.product_name, c.name 
 from product as p 
-inner join category as c on p.product_id =  c.category_id;
+inner join category as c on p.category_id =  c.category_id;
 
 /**
  * Exercise #5
@@ -32,19 +32,23 @@ select pu.purchase_id, p.product_name, pu_i.unit_price, pu_i.quantity
 from product as p
 inner join purchase_item as pu_i on p.product_id = pu_i.product_id
 inner join  purchase as pu on pu.purchase_id =  pu_i.purchase_id;
+
 /**
  * Exercise #6
  */
-select p.category_id, c.category_name 
+select  c.name, pu.purchase_id
 from category as c
 inner join product as p on c.category_id = p.product_id
+inner join purchase_item as pu_i on pu_i.purchase_id = p.product_id
+inner join purchase as pu on pu.purchase_id = pu_i.purchase_id
+group by c.name, pu.purchase_id;;
 
 /**
  * Exercise #7
  */
 select * 
 from category 
-order by category_name asc;
+order by name asc;
 
 /**
  * Exercise #8
@@ -63,12 +67,13 @@ order by quantity_per_unit desc, product_name asc;
 /**
  * Exercise #10
  */
-select product_name, avg(unit_price) 
-FROM product
-group by product_name
+select c.name, avg(unit_price) as average_unit_price
+FROM category as c
+inner join product as p on c.category_id = p.category_id
+group by c.name;
 
 /**
- * Exercise #11
+ * Exercise #11(COMMING SOON)
  */
 select c.city, count(customer_id) as total
 from store.customer AS c
